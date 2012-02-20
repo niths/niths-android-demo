ad678000c99c09423f2b2a8fd3ebaffd3dbccc1a
whirlwin <whirlwin90@gmail.com>
2012-02-20 16:48:39 +0100

    Shortened down the programme.

--- src/no/niths/Main.java
+++ src/no/niths/Main.java
@@ -1,26 +1,51 @@
 package no.niths;
 
+import java.io.IOException;
+
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.api.client.googleapis.auth.oauth2.draft10.GoogleAccessProtectedResource;
+import com.google.api.client.googleapis.extensions.android2.auth.GoogleAccountManager;
+
 public class Main extends Activity {
-    /** Called when the activity is first created. */
+    
+    GoogleAccessProtectedResource resource = new GoogleAccessProtectedResource(null);
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        AccountManager manager = AccountManager.get(this);
-        Account[] accounts = manager.getAccountsByType("com.google");
-
-        for (Account account : accounts) {
-	        Toast.makeText(this, account.name, Toast.LENGTH_LONG).show();
-        }
-        
-        startActivity(new Intent(this, TasksSample.class));
+        GoogleAccountManager googleAccountManager = new GoogleAccountManager(this);
+        Account account = googleAccountManager.getAccountByName("degyvi09@nith.no");
+         
+        String authType = "oauth2:https://www.googleapis.com/auth/userinfo.email";
+         
+        googleAccountManager.manager.getAuthToken(account, authType, true,
+                new AccountManagerCallback<Bundle>() {
+                    
+                    public void run(AccountManagerFuture<Bundle> arg0) {
+                        try {
+                            resource.setAccessToken(arg0.getResult().getString(AccountManager.KEY_AUTHTOKEN));
+                            String token = resource.getAccessToken();
+                            
+                            Toast.makeText(Main.this, "token -> " + token, Toast.LENGTH_LONG).show();
+                        } catch (OperationCanceledException e) {
+                            e.printStackTrace();
+                        } catch (AuthenticatorException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, null);
     }
 }
