41858471d5c228dd2b0f501440afd061bae0da79
whirlwin <whirlwin90@gmail.com>
2012-02-20 18:02:45 +0100

    Made it work, and I'm hungry.

--- src/no/niths/Main.java
+++ src/no/niths/Main.java
@@ -1,6 +1,15 @@
 package no.niths;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
@@ -10,6 +19,7 @@ import android.accounts.AuthenticatorException;
 import android.accounts.OperationCanceledException;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.api.client.googleapis.auth.oauth2.draft10.GoogleAccessProtectedResource;
@@ -36,13 +46,30 @@ public class Main extends Activity {
                         try {
                             resource.setAccessToken(arg0.getResult().getString(AccountManager.KEY_AUTHTOKEN));
                             String token = resource.getAccessToken();
-                            
                             Toast.makeText(Main.this, "token -> " + token, Toast.LENGTH_LONG).show();
+                            
+
+                            HttpClient client = new DefaultHttpClient();
+                            HttpPost post = new HttpPost(
+                                    "http://10.0.2.2:8080/niths/auth-user-token");
+                            List<NameValuePair> data =
+                                    new ArrayList<NameValuePair>();
+                            data.add(new BasicNameValuePair("token", token));
+                            post.setEntity(new UrlEncodedFormEntity(data));
+                            
+                            org.apache.http.HttpResponse response = client.execute(post);
+                            
+                            Log.i("token:", "" + token);
+                            
+                            
                         } catch (OperationCanceledException e) {
+                            Log.e("errs", e.getMessage());
                             e.printStackTrace();
                         } catch (AuthenticatorException e) {
+                            Log.e("errs", e.getMessage());
                             e.printStackTrace();
                         } catch (IOException e) {
+                            Log.e("errs", e.getMessage());
                             e.printStackTrace();
                         }
                     }
