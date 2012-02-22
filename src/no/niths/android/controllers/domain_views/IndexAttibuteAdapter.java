cc9c771da07ced6442259a11b80462da968b73f8
whirlwin <whirlwin90@gmail.com>
2012-02-22 23:08:13 +0100

    Fixed a bug when displaying dead views.

--- src/no/niths/android/controllers/domain_views/IndexAttibuteAdapter.java
+++ src/no/niths/android/controllers/domain_views/IndexAttibuteAdapter.java
@@ -12,7 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 /**
@@ -20,14 +20,15 @@ import android.widget.TextView;
  * @author NITHs
  *
  */
-public class IndexAttibuteAdapter extends BaseAdapter {
+public class IndexAttibuteAdapter extends ArrayAdapter<Field> {
     private ArrayList<Field> list;
     private LayoutInflater inflater;
     private Domain domain;
     
     public IndexAttibuteAdapter(Context context,
-            ArrayList<Field> applicationList) {
-        this.list = applicationList;
+            ArrayList<Field> list) {
+        super(context, R.layout.attributes_list, list);
+        this.list = list;
         inflater = LayoutInflater.from(context);
     }
 
@@ -39,7 +40,7 @@ public class IndexAttibuteAdapter extends BaseAdapter {
         return list.size();
     }
 
-    public Object getItem(int index) {
+    public Field getItem(int index) {
         return list.get(index);
     }
 
@@ -52,27 +53,23 @@ public class IndexAttibuteAdapter extends BaseAdapter {
 
     // Each view in the element list is represented by this
     public View getView(int index, View convertView, ViewGroup parent) {
-        ViewHolder nameHolder;
-        ViewHolder valueHolder;
-        
-        if(convertView == null) {
+        if (convertView == null) {
             convertView = inflater.inflate(R.layout.attributes_list, null);
-            nameHolder = new ViewHolder();
-            nameHolder.tevName =
-                    (TextView) convertView.findViewById(R.id.tevAttributeName);
-            valueHolder = new ViewHolder();
-            valueHolder.tevValue =
-                    (TextView) convertView.findViewById(R.id.tevAttributeValue);
-            convertView.setTag(nameHolder);
-            convertView.setTag(valueHolder);
-        } else {
-            nameHolder = (ViewHolder) convertView.getTag();
-            valueHolder = (ViewHolder) convertView.getTag();
+            
         }
+
+        ViewHolder nameHolder = new ViewHolder();
+        nameHolder.tevName =
+                (TextView) convertView.findViewById(R.id.tevAttributeName);
+        ViewHolder valueHolder = new ViewHolder();
+        valueHolder.tevValue =
+                (TextView) convertView.findViewById(R.id.tevAttributeValue);
+        convertView.setTag(nameHolder);
+        convertView.setTag(valueHolder);
 
         // The current field, e.g. name, id, etc.
         Field field = list.get(index);
-
+        
         nameHolder.tevName.setText(field.getName());
         valueHolder.tevValue.setText(getValue(field));
 
@@ -93,6 +90,7 @@ public class IndexAttibuteAdapter extends BaseAdapter {
         try {
             // TODO
             // Remove warnings
+            Log.i("method call", ""+name);
             Method m = domain.getClass().getMethod("get" + name, null);
             result = m.invoke(domain, null);
         } catch (SecurityException e) {
@@ -107,8 +105,7 @@ public class IndexAttibuteAdapter extends BaseAdapter {
             Log.e(String.valueOf(R.string.reflection_error), e.getMessage());
         }
 
-        String value = result.toString();
-        return value == null ? "null" : value;
+        return result == null ? "null" : result.toString();
     }
 
     static class ViewHolder {
